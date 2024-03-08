390f0c0a8dc1168ea6f0894868da37594edc8f6d
LungeloDev <lungelompanza.dev@gmail.com>
2024-03-08 15:03:02 +0200

    fix deploy

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'umqhubifrontend.web.app',
+    siteUrl: 'https://umqhubi-frontend.netlify.app',
     generateRobotsTxt: true, // (optional)
     outDir: './out'
 }
